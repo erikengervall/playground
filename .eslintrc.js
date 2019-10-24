ef271fdd2767d0e84c9ac737a880e0ebea875783
Erik Engervall <erik.engervall@gmail.com>
2019-10-24 22:11:33 +0200

    azure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,15 @@
 module.exports = {
   env: {
+    commonjs: true,
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
   rules: {},
 }

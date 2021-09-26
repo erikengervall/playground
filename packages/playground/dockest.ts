498eaeebcdcf02a4527ff9cdd5c5357b83c51880
Erik Engervall <erik.engervall@gmail.com>
2021-09-26 13:48:05 +0200

    clean slate

---
+++ packages/playground/dockest.ts
@@ -0,0 +1,12 @@
+import { Dockest } from 'dockest'
+
+const file = __dirname + '/docker-compose.yml'
+console.log({ file })
+
+const dockest = new Dockest({
+  composeFile: file,
+})
+
+const dockestServices = [{ serviceName: 'myRedis' }]
+
+dockest.run(dockestServices)

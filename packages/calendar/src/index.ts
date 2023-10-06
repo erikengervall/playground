a13fc0c91d1f3f1a5d5ca59f67daa1f9581cbb58
Erik Engervall <erik.engervall@gmail.com>
2023-10-06 09:42:09 +0900

    calendar

--- packages/calendar/src/index.ts
+++ packages/calendar/src/index.ts
@@ -1,4 +1,22 @@
-export default () => {
-  //
-  console.log('@engervall/playgroud-2 ✌🏼 ');
-};
+import ical from 'node-ical';
+
+const icsAbsoluteFilePath = process.argv[2];
+const events = ical.sync.parseFile(icsAbsoluteFilePath);
+
+export function main() {
+  for (const [, event] of Object.entries(events)) {
+    if (event.type === 'VEVENT') {
+      console.log(
+        'Summary: ' +
+          event.summary +
+          '\nDescription: ' +
+          event.description +
+          '\nStart Date: ' +
+          event.start.toISOString() +
+          '\n'
+      );
+    }
+  }
+}
+
+main();
